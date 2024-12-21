da56a9302a0fbafecfcb6f905a7b54af802b81fe
Raphael Stellwag <r.stellwag@gmx.net>
2024-12-21 12:09:37 +0800

    Add model_template.py

---
+++ model_template.py
@@ -0,0 +1,15 @@
+
+
+class model_template:
+
+    def train(self, path_to_data):
+        pass
+
+    def save_model(self, save_location):
+        pass
+
+    def load_model(self, load_location):
+        pass
+
+    def predict(self, image_data):
+        pass
