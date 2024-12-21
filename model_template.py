1d235682819f2b7e03d0bfb22550e24270a3a52c
Raphael Stellwag <r.stellwag@gmx.net>
2024-12-21 13:00:32 +0800

    Add default implementation for predict

--- model_template.py
+++ model_template.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 
 class ModelTemplate:
@@ -12,5 +13,24 @@ class ModelTemplate:
     def load_model(self):
         pass
 
-    def predict(self, image_data):
+    def predict(self, img):
+
+        target_size = (224, 224)
+
+        # Resize the image
+        img = img.resize(target_size)
+        # Convert the image to a numpy array
+        img_array = np.array(img)
+        # Add batch dimension
+        img_array = np.expand_dims(img_array, axis=0)
+        # Scale the image values to [0, 1]
+        img_array = img_array.astype('float32') / 255.
+
+
+        # Load the model
+        # predictions = model.predict(img_array)
+        # predicted_class_index = np.argmax(predictions, axis=1)[0]
+        # predicted_class_name = class_indices[predicted_class_index]
+        # return predicted_class_name
+
         return "Prediction"
