fc71dbdede11dfd3d349efb00ab69238e5d799e1
Raphael Stellwag <r.stellwag@gmx.net>
2025-01-06 16:23:00 +0800

    Add documentation

--- main_app.py
+++ main_app.py
@@ -13,33 +13,42 @@ warnings.filterwarnings("ignore")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    last_selected_model: ModelTemplate = ModelTemplate()
-    last_selected_model.load_model()
 
+    # All available models --> Easy to add new models
     models = [ModelCNN(), ResNetModel(), EfficientNetV2SModel()]
-    model_names = list()
 
+    # Retrieve the names of the models
+    model_names = list()
     for model in models:
         model_names.append(model.name)
 
+    # By default, the first model is selected
+    last_selected_model: ModelTemplate = models[0]
+    last_selected_model.load_model()
+
     st.title('Plant Disease Classification')
 
+    # Streamlit file uploader for image file
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
+    # Streamlit selectbox for model selection
     option = st.selectbox(
         "What model do you like to use?",
         (model_names)
     )
 
+    # If an image is uploaded and a model is selected
     if uploaded_file is not None and option is not None:
 
         selected_model = models[model_names.index(option)]
 
+        # If the model was changed we have to load the new model
         if last_selected_model != selected_model:
             last_selected_model.unload_model()
             selected_model.load_model()
             last_selected_model = selected_model
 
+        # Display the uploaded image
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image.', use_column_width=True)
 
@@ -47,6 +56,7 @@ if __name__ == '__main__':
             # Display "Classifying..." initially
             st.write("Classifying...")
 
+            # Predict the image, prediction is a string and contains the class
             prediction = selected_model.predict(image)
 
             # Update the placeholder with the prediction result
